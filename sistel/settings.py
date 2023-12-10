dd2d6d0d491e236cae457ca71d214df9a5bdb0c9
isaui <isadestroyed17@gmail.com>
2023-12-10 16:53:03 +0700

    resolve conflict update all #3

--- sistel/settings.py
+++ sistel/settings.py
@@ -85,17 +85,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sistel.wsgi.application'
 #ASGI_APPLICATION = 'sistel.asgi.application' 
 
-#Tidak pakai db django
+#tapi tidak dipakai dengan orm django
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'sistelproject_dayablewhy',                   
+        'USER': 'sistelproject_dayablewhy',                   
+        'PASSWORD': '15eaebb5beb4f3e1c03e5b1e708da41579aaef3f',           
+        'HOST': 'eum.h.filess.io',                       
+        'PORT': '5432',                            
     }
 }
+
 
 
 # Password validation
